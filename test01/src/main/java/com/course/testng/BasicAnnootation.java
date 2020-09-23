fd5234f7cdc3053b35247fb26f907e9d5c38483d
@zhihua1986 <lhlaishui@163.com>
2020-09-23 09:10:47 +0800

    更新代码

---
+++ test01/src/main/java/com/course/testng/BasicAnnootation.java
@@ -0,0 +1,14 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnootation {
+	@Test
+	public void testcode01()
+	{
+		System.out.println("这是TE");
+		
+	}
+
+}
+
